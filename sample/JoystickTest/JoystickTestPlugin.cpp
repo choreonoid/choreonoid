b24f25f69abba5dcf38eb340a2314402a7416b92
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2014-04-27 22:53:31 +0900

    add JoystickTestPlugin, a sample for using the JoystickCapture class

---
+++ sample/JoystickTest/JoystickTestPlugin.cpp
@@ -0,0 +1,48 @@
+/*!
+  @author Shin'ichiro Nakaoka
+*/
+
+#include <cnoid/Plugin>
+#include <cnoid/JoystickCapture>
+#include <cnoid/MessageView>
+#include <boost/bind.hpp>
+
+using namespace boost;
+using namespace cnoid;
+
+namespace {
+
+class JoystickTestPlugin : public Plugin
+{
+    JoystickCapture joystick;
+    MessageView* mv;
+    
+public:
+    JoystickTestPlugin() : Plugin("JoystickTest") { }
+    
+    virtual bool initialize() {
+
+        joystick.setDevice("/dev/input/js0");
+
+        joystick.sigButton().connect(
+            bind(&JoystickTestPlugin::onButtonEvent, this, _1, _2));
+        joystick.sigAxis().connect(
+            bind(&JoystickTestPlugin::onAxisEvent, this, _1, _2));
+
+        mv = MessageView::instance();
+            
+        return true;
+    }
+
+    void onButtonEvent(int id, double position) {
+        mv->putln(format("Joystick button %1%: %2%") % id % position);
+    }
+
+    void onAxisEvent(int id, double position) {
+        mv->putln(format("Joystick axis %1%: %2%") % id % position);
+    }
+};
+
+}
+
+CNOID_IMPLEMENT_PLUGIN_ENTRY(JoystickTestPlugin);
