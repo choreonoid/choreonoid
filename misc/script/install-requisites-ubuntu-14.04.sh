227bcf56f6151724c5fb6b8e21dc11cd53e8c8fa
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2014-05-06 09:29:26 +0900

    add the package install script for Ubuntu 14.04

---
+++ misc/script/install-requisites-ubuntu-14.04.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+sudo apt-get install \
+build-essential \
+cmake-curses-gui \
+libboost-all-dev \
+libeigen3-dev \
+libeigen3-doc \
+libqt4-dev \
+libqt4-opengl-dev \
+qt4-dev-tools \
+qt4-qtconfig \
+qt4-doc-html \
+libglew-dev \
+libyaml-dev \
+gettext \
+zlib1g-dev \
+libjpeg-dev \
+libpng12-dev \
+libode-dev \
+libomniorb4-dev \
+libcos4-dev \
+omniidl \
+omniorb-nameserver \
+python-omniorb \
+omniidl-python \
+libgstreamer0.10-dev \
+libgstreamer-plugins-base0.10-dev \
+libpulse-dev \
+libsndfile1-dev \
+python2.7-dev \
+uuid-dev \
+libshiboken-dev \
+libpyside-dev
--- src/Util/MeshGenerator.h
+++ src/Util/MeshGenerator.h
@@ -3,8 +3,8 @@
   @author Shin'ichiro Nakaoka
 */
 
-#ifndef CNOID_UTIL_MESH_GENERATOR_H_INCLUDED
-#define CNOID_UTIL_MESH_GENERATOR_H_INCLUDED
+#ifndef CNOID_UTIL_MESH_GENERATOR_H
+#define CNOID_UTIL_MESH_GENERATOR_H
 
 #include "SceneShape.h"
 #include "exportdecl.h"
