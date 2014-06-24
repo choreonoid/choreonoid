54e90a4ce66fb70092643251ecd6b07b8bca62c5
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2014-06-24 19:07:36 +0900

    Rectangle for removing a point set can be specified on a SceneView

--- src/Base/PointSetItem.cpp
+++ src/Base/PointSetItem.cpp
@@ -22,11 +22,25 @@ using namespace cnoid;
 
 namespace {
 
+class RectLineOverlay : public SgOverlay
+{
+public:
+    SgVertexArrayPtr vertices;
+    RectLineOverlay();
+    virtual void calcViewVolume(double viewportWidth, double viewportHeight, ViewVolume& io_volume);
+    void setRect(int x0, int y0, int x1, int y1);
+};
+
+typedef ref_ptr<RectLineOverlay> RectLineOverlayPtr;
+
+
 class PointSetEraser : public SceneWidgetEditable, public Referenced
 {
 public:
     SceneWidget* sceneWidget;
     boost::signals::connection connection;
+    RectLineOverlayPtr rectLineOverlay;
+    int x0, y0;
 
     PointSetEraser(SceneWidget* sceneWidget);
     void onSceneWidgetAboutToBeDestroyed();
@@ -396,6 +410,8 @@ bool PointSetItem::restore(const Archive& archive)
 PointSetEraser::PointSetEraser(SceneWidget* sceneWidget)
   : sceneWidget(sceneWidget)
 {
+    rectLineOverlay = new RectLineOverlay;
+    
     sceneWidget->installEventFilter(this);
     connection = sceneWidget->sigAboutToBeDestroyed().connect(
         boost::bind(&PointSetEraser::onSceneWidgetAboutToBeDestroyed, this));
@@ -411,6 +427,9 @@ void PointSetEraser::onSceneWidgetAboutToBeDestroyed()
 PointSetEraser::~PointSetEraser()
 {
     if(sceneWidget){
+        if(rectLineOverlay->hasParents()){
+            sceneWidget->sceneRoot()->removeChild(rectLineOverlay, true);
+        }
         sceneWidget->removeEventFilter(this);
         connection.disconnect();
     }
@@ -419,23 +438,84 @@ PointSetEraser::~PointSetEraser()
 
 bool PointSetEraser::onButtonPressEvent(const SceneWidgetEvent& event)
 {
-    //cout << "PointSetEraser::onButtonPressEvent()" << endl;
+    x0 = event.x();
+    y0 = event.y();
+    rectLineOverlay->setRect(x0, y0, x0, y0);
+    
+    if(!rectLineOverlay->hasParents()){
+        sceneWidget->sceneRoot()->addChild(rectLineOverlay, true);
+    }
 }
 
 
 bool PointSetEraser::onButtonReleaseEvent(const SceneWidgetEvent& event)
 {
-    //cout << "PointSetEraser::onButtonReleaseEvent()" << endl;
+
 }
 
 
 bool PointSetEraser::onPointerMoveEvent(const SceneWidgetEvent& event)
 {
-    //cout << "PointSetEraser::onPointerMoveEvent()" << endl;
+    rectLineOverlay->setRect(x0, y0, event.x(), event.y());
 }
 
 
 void PointSetEraser::onContextMenuRequest(const SceneWidgetEvent& event, MenuManager& menuManager)
 {
-    //cout << "PointSetEraser::onContextMenuRequest()" << endl;
+
+}
+
+
+RectLineOverlay::RectLineOverlay()
+{
+    SgLineSet* lineSet = new SgLineSet;
+    vertices = lineSet->getOrCreateVertices();
+    vertices->resize(4);
+    lineSet->getOrCreateColors()->push_back(Vector3f(1.0f, 0.0f, 0.0f));
+    lineSet->reserveNumLines(4);
+    SgIndexArray& colorIndices = lineSet->colorIndices();
+    colorIndices.reserve(8);
+    for(int i=0; i < 4; ++i){
+        lineSet->addLine(i, (i + 1) % 4);
+        colorIndices.push_back(0);
+        colorIndices.push_back(0);
+    }
+    addChild(lineSet);
+}
+
+
+void RectLineOverlay::calcViewVolume(double viewportWidth, double viewportHeight, ViewVolume& io_volume)
+{
+    io_volume.left = 0;
+    io_volume.right = viewportWidth;
+    io_volume.bottom = 0;
+    io_volume.top = viewportHeight;
+}
+
+
+void RectLineOverlay::setRect(int x0, int y0, int x1, int y1)
+{
+    float left, right, top, bottom;
+
+    if(x0 <= x1){
+        left = x0;
+        right = x1;
+    } else {
+        left = x1;
+        right = x0;
+    }
+    if(y0 >= y1){
+        top = y0;
+        bottom = y1;
+    } else {
+        top = y1;
+        bottom = y0;
+    }
+
+    vertices->at(0) << left,  top,    0.0f;
+    vertices->at(1) << left,  bottom, 0.0f;
+    vertices->at(2) << right, bottom, 0.0f;
+    vertices->at(3) << right, top,    0.0f;
+
+    vertices->notifyUpdate();
 }
