5aa8f2a4c0c773d7e98c8e25463497a1f6fa76fa
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2014-07-09 19:33:47 +0900

    add the parameter to specify volume to the 'playAudioFile' function

--- src/MediaPlugin/MediaUtil.h
+++ src/MediaPlugin/MediaUtil.h
@@ -2,14 +2,16 @@
    @author Shin'ichiro Nakaoka
 */
 
-#ifndef CNOID_MEDIA_PLUGIN_MEDIA_UTIL_H_INCLUDED
-#define CNOID_MEDIA_PLUGIN_MEDIA_UTIL_H_INCLUDED
+#ifndef CNOID_MEDIA_PLUGIN_MEDIA_UTIL_H
+#define CNOID_MEDIA_PLUGIN_MEDIA_UTIL_H
 
 #include <string>
 #include "exportdecl.h"
 
 namespace cnoid {
-CNOID_EXPORT bool playAudioFile(const std::string& filename);
+
+CNOID_EXPORT bool playAudioFile(const std::string& filename, double volumeRatio = -1.0);
+
 }
 
 #endif
