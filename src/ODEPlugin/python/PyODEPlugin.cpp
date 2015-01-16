c7da17c9239ef3a0f0a9a4fc35da7780d9e81b11
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2015-01-16 18:03:16 +0900

    rewrite the python wrapper of ODEPlugin

--- src/ODEPlugin/python/PyODEPlugin.cpp
+++ src/ODEPlugin/python/PyODEPlugin.cpp
@@ -1,47 +1,20 @@
 /*!
- * @author Hisashi Ikari
- */
-#include <boost/python.hpp>
+  @author Shin'ichiro Nakaoka
+*/
 
-// This header is an "EXPLICIT" item of dependence.
-// And this items are located in the "cnoid/include" or same directory.
-// Its meaning is exposed externally. For reason, we can will be selected here.
-
-#include <cnoid/Referenced>
-#include <cnoid/Item>
-#include <cnoid/RootItem>
-#include <cnoid/WorldItem>
-#include <cnoid/SimulatorItem>
-
-#include "../../Base/python/PyBase.h"
 #include "../ODESimulatorItem.h"
+#include <cnoid/PyBase>
 
-// We recommend the use of minieigen.
 using namespace boost::python;
 using namespace cnoid;
 
-namespace cnoid
+BOOST_PYTHON_MODULE(ODEPlugin)
 {
-    /*!
-     * @brief Reference types are explicitly declare a function pointer. 
-     *        Reference types share a reference to the original(in C++).
-     *        But, it does not share the type of destination(in python).
-     *        (Reason-1. Primitive types can not be a reference type.)
-     *        (Reason-2. Minieigen create an object always.)
-     *        Therefore, we must use the setter if you want to use the python.
-     *        We will define the settings of the following variables.       
-     */
-    BOOST_PYTHON_MODULE(ODEPlugin)
+    boost::python::import("cnoid.BodyPlugin");
+
     {
-        /*!
-         * @brief Define the interface for ODESimulatorItem.
-         * @note  !!! IMPORTANT !!! Please load the cnoid.BodyPlugin before the reading of this module. 
-         *                          BodyPlugin loads the SimulatorItem.
-         */
-        class_ < ODESimulatorItem, ref_ptr<ODESimulatorItem>, 
-            bases<SimulatorItem>, boost::noncopyable >("ODESimulatorItem", init<>())
-            .def("__init__", boost::python::make_constructor(&createInstance<ODESimulatorItem>))
-            .def("setAllLinkPositionOutputMode", &ODESimulatorItem::setAllLinkPositionOutputMode)
+        scope odeSimulatorItemScope = 
+            class_< ODESimulatorItem, ODESimulatorItemPtr, bases<SimulatorItem> >("ODESimulatorItem")
             .def("setStepMode", &ODESimulatorItem::setStepMode)
             .def("setGravity", &ODESimulatorItem::setGravity)
             .def("setFriction", &ODESimulatorItem::setFriction)
@@ -54,17 +27,16 @@ namespace cnoid
             .def("setCorrectingVelocityLimitMode", &ODESimulatorItem::setCorrectingVelocityLimitMode)
             .def("setMaxCorrectingVelocity", &ODESimulatorItem::setMaxCorrectingVelocity)
             .def("setSurfaceLayerDepth", &ODESimulatorItem::setSurfaceLayerDepth)
-            .def("useWorldCollisionDetector", &ODESimulatorItem::useWorldCollisionDetector);
- 
+            .def("useWorldCollisionDetector", &ODESimulatorItem::useWorldCollisionDetector)
+            ;
 
-        /*!
-         * @brief Provides following enum value of the BodyItem.
-         */
-        enum_ <ODESimulatorItem::StepMode>("ODEStepMode")
+        enum_<ODESimulatorItem::StepMode>("StepMode")
             .value("STEP_ITERATIVE", ODESimulatorItem::STEP_ITERATIVE)
             .value("STEP_BIG_MATRIX", ODESimulatorItem::STEP_BIG_MATRIX)
             .value("NUM_STEP_MODES", ODESimulatorItem::NUM_STEP_MODES);    
-        
     }
 
-}; // end of namespace
+    implicitly_convertible<ODESimulatorItemPtr, SimulatorItemPtr>();
+    PyItemList<ODESimulatorItem>("ODESimulatorItemList");
+}
+
