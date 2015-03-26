8e75d8d1c76a10850465a1f46fe823eaffc9e280
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2015-03-26 12:27:49 +0900

    modify the API of AbstractTaskProcessor and rename it to be AbstractTaskSequencer

--- src/Util/AbstractTaskSequencer.h
+++ src/Util/AbstractTaskSequencer.h
@@ -3,17 +3,19 @@
   @author Shin'ichiro Nakaoka
 */
 
-#ifndef CNOID_UTIL_ABSTRACT_TASK_PROCESSOR_H
-#define CNOID_UTIL_ABSTRACT_TASK_PROCESSOR_H
+#ifndef CNOID_UTIL_ABSTRACT_TASK_SEQUENCER_H
+#define CNOID_UTIL_ABSTRACT_TASK_SEQUENCER_H
 
 #include "Task.h"
 #include "exportdecl.h"
 
 namespace cnoid {
 
-class CNOID_EXPORT AbstractTaskProcessor
+class CNOID_EXPORT AbstractTaskSequencer
 {
 public:
+    virtual ~AbstractTaskSequencer();
+    
     virtual void addTask(Task* task) = 0;
     virtual bool updateTask(Task* task) = 0;
     virtual int numTasks() const = 0;
@@ -21,13 +23,25 @@ public:
     virtual int currentTaskIndex() const = 0;
     virtual SignalProxy<void(int index)> sigCurrentTaskIndexChanged() = 0;
     virtual int currentPhaseIndex() const = 0;
+
+    /**
+       \note This signal is emitted when the current task is changed as well.
+    */
     virtual SignalProxy<void(int index)> sigCurrentPhaseIndexChanged() = 0;
+
+    /*
+      @return The -1 value is returned when the current command is an implicit pre-processing command.
+    */
     virtual int currentCommandIndex() const = 0;
+
+    /**
+       \note This signal is emitted when the current task or phase is changed as well.
+    */       
     virtual SignalProxy<void(int index)> sigCurrentCommandIndexChanged() = 0;
 
-    enum CommandState { IDLE, BUSY };
-    virtual int commandState() const = 0;
-    virtual SignalProxy<void(int state)> sigCommandStateChanged() = 0;
+    virtual bool isBusy() const = 0;
+    virtual SignalProxy<void(bool isBusy)> sigBusyStateChanged() = 0;
+    virtual bool isAutoMode() const = 0;
 };
 
 }
