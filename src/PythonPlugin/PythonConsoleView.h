fcf6950ae85c241ceb6eab2952ef62eb0d7acad9
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2015-05-23 13:10:34 -0700

    export the symbols of the PythonConsoleView class

--- src/PythonPlugin/PythonConsoleView.h
+++ src/PythonPlugin/PythonConsoleView.h
@@ -2,16 +2,17 @@
    @author Shin'ichiro Nakaoka
 */
 
-#ifndef CNOID_PYTHON_PLUGIN_PYTHON_CONSOLE_VIEW_H_INCLUDED
-#define CNOID_PYTHON_PLUGIN_PYTHON_CONSOLE_VIEW_H_INCLUDED
+#ifndef CNOID_PYTHON_PLUGIN_PYTHON_CONSOLE_VIEW_H
+#define CNOID_PYTHON_PLUGIN_PYTHON_CONSOLE_VIEW_H
 
 #include <cnoid/View>
+#include "exportdecl.h"
 
 namespace cnoid {
 
 class PythonConsoleViewImpl;
 
-class PythonConsoleView : public View
+class CNOID_EXPORT PythonConsoleView : public View
 {
 public:
     static void initializeClass(ExtensionManager* ext);
@@ -23,9 +24,11 @@ public:
     void putln(const std::string& message);
     void flush();
     void clear();
+    
 private:
     PythonConsoleViewImpl* impl;
 };
+
 }
 
 #endif
