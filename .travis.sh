91e73ccd229693c4eef67274021b1bc4cce0c55c
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-01-29 18:36:22 +0900

    updates required packages

--- .travis.sh
+++ .travis.sh
@@ -3,13 +3,11 @@
 set -x
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu precise main" > /etc/apt/sources.list.d/drc-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-wget http://packages.osrfoundation.org/drc.key -O - | sudo apt-key add -
 
 sudo add-apt-repository -y ppa:hrg/daily
 sudo apt-get update -qq
-sudo apt-get install -qq -y choreonoid drcsim-hydro
+sudo apt-get install -qq -y libcnoid-dev libsdformat-dev
 
 source /opt/ros/hydro/setup.bash
 source /usr/share/drcsim/setup.sh
