7fe75ba16395ffe17374d2887e8a38239d1f33f6
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-07-06 16:17:31 +0900

    returns 1.0 as marker radius if bounding box is empty

--- src/BodyPlugin/EditableSceneBody.cpp
+++ src/BodyPlugin/EditableSceneBody.cpp
@@ -26,6 +26,9 @@
 #include <cnoid/ConnectionSet>
 #include <boost/bind.hpp>
 #include "gettext.h"
+#if 1
+#include <iostream>
+#endif
 
 using namespace std;
 using namespace cnoid;
@@ -405,6 +408,7 @@ EditableSceneBodyImpl::EditableSceneBodyImpl(EditableSceneBody* self, BodyItemPt
 double EditableSceneBodyImpl::calcLinkMarkerRadius(SceneLink* sceneLink) const
 {
     const BoundingBox& bb = sceneLink->visualShape()->boundingBox();
+    if (bb.empty()) return 1.0; // Is this OK?
     double V = ((bb.max().x() - bb.min().x()) * (bb.max().y() - bb.min().y()) * (bb.max().z() - bb.min().z()));
     return pow(V, 1.0 / 3.0) * 0.6;
 }
