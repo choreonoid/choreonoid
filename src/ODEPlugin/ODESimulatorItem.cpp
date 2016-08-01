c5994138444ebe9af660896b4a4c309b01c37c2c
Shizuko Hattori <s.hattori@aist.go.jp>
2016-08-01 10:48:03 +0900

    ODE表面速度のパラメータ変更

--- src/ODEPlugin/ODESimulatorItem.cpp
+++ src/ODEPlugin/ODESimulatorItem.cpp
@@ -1191,7 +1191,7 @@ static void nearCallback(void* data, dGeomID g1, dGeomID g2)
                     if(contacts[i].geom.depth > 0.001){
                         continue;
                     }
-                    surface.mode = dContactFDir1 | dContactMotion1 | dContactMu2 | dContactApprox1_2;
+                    surface.mode = dContactFDir1 | dContactMotion1 | dContactMu2 | dContactApprox1_2 | dContactApprox1_1;
                     const Vector3 axis = crawlerlink->R() * crawlerlink->a();
                     const Vector3 n(contacts[i].geom.normal);
                     Vector3 dir = axis.cross(n);
