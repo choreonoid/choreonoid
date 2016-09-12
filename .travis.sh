a329df4cc491cce654eee431d44dcf640d5e3ad2
Hiroyuki MATSUSHITA <mat@soum.co.jp>
2016-09-12 21:18:22 +0900

    adjust .travis files

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
 sudo add-apt-repository -y ppa:hrg/daily
