27969d30e5c9459cb830be456596c38f1465defd
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2016-10-28 23:24:09 +0900

    Improve the way to define the cnoid.derive function in Lua

--- src/Util/lua/LuaUtilModule.cpp
+++ src/Util/lua/LuaUtilModule.cpp
@@ -22,37 +22,35 @@ extern "C" CNOID_EXPORT int luaopen_cnoid_Util(lua_State* L)
     sol::table module = lua.create_table();
 
     /**
-       \note It is desriable to directly obtain the defined function,
-       but the return value of the script does not seems to be pushed to the stack
-       and the value is not correctly retrieved, so the global variable is currently
-       used to retireve the defined function
+       \note The lua.script() function cannot be used here because it has a bug
+       in which the return value cannot be obtained if the stack is not empty
+       when the function is called.
     */
-    //sol::object result =
-    lua.script(
-        //"return function(baseClass) "
-        "createDerivedClass = function(baseClass) "
+    sol::stack::script(
+        L,
+        "return function(baseClass) "
         "  local class = { } "
         "  class.new = function(...) "
-        "  local obj = { } "
-        "  baseobj = baseClass.new(...) "
-        "  if type(baseobj) == \"table\" then "
-        "    obj.cppobj = baseobj.cppobj "
-        "  else "
-        "    obj.cppobj = baseobj "
-        "  end "
-        "  obj.super = baseobj "
-        "  setmetatable(obj, { __index = class }) "
-        "  obj.cppobj:setDerivedLuaObject(obj) "
-        "  class.initialize(obj) "
+        "    local obj = { } "
+        "    baseobj = baseClass.new(...) "
+        "    if type(baseobj) == \"table\" then "
+        "      obj.cppobj = baseobj.cppobj "
+        "    else "
+        "      obj.cppobj = baseobj "
+        "    end "
+        "    obj.super = baseobj "
+        "    setmetatable(obj, { __index = class }) "
+        "    obj.cppobj:setDerivedLuaObject(obj) "
+        "    class.initialize(obj) "
         "    return obj "
         "  end "
         "  setmetatable(class, { __index = baseClass }) "
         "  return class "
         "end "
         );
-
-    //module["derive"] = result;
-    module["derive"] = lua["createDerivedClass"];
+    sol::stack_object deriveFunction(L);
+    module["derive"] = deriveFunction;
+    lua_pop(L, 1);
 
     exportLuaSignalTypes(module);
     exportLuaTaskTypes(module);
