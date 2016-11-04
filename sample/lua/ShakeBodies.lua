9d43734efb976ea42c38a907e0100f03b7f421f9
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2016-11-04 23:50:04 +0900

    Improve the slot call iteration to avoid a crash when a slot is disconnected during the iteration

--- sample/lua/ShakeBodies.lua
+++ sample/lua/ShakeBodies.lua
@@ -3,61 +3,60 @@ cnoid.require "Base"
 cnoid.require "Body"
 cnoid.require "BodyPlugin"
 
-BodyShaker = {
-   new = function()
-      local self = { }
-      self.toolBar = cnoid.ToolBar.new("ShakeBar")
-      self.toolBar:setVisibleByDefault(true)
-      self.button = self.toolBar:addToggleButton("Shake")
-      self.button:setChecked(true)
-
-      self.bodyItems = { }
-      self.dp = cnoid.Vector3(0.0, 0.0, 0.01)
-      self.connections = cnoid.ScopedConnectionSet.new()
-      self.timer = cnoid.Timer.new()
-
-      function onSelectionChanged(items)
-	 self.bodyItems = cnoid.extractBodyItems(items)
-	 if #self.bodyItems > 0 and self.button:isChecked() then
-	    self.timer:start(50)
-	 else
-	    self.timer:stop()
-	 end
-      end
-
-      self.connections:add(
-	 cnoid.ItemTreeView.instance():sigSelectionChanged():connect(onSelectionChanged))
-
-      self.connections:add(
-	 self.button:sigToggled():connect(
-	    function(on)
-	       onSelectionChanged(cnoid.ItemTreeView.instance():selectedItems())
-	    end))
-
-      self.onTimeout = function()
-	 for i, bodyItem in ipairs(self.bodyItems) do
-	    local body = bodyItem:body()
-	    body:rootLink():setTranslation(body:rootLink():translation() + self.dp)
-	    body:calcForwardKinematics()
-	    bodyItem:notifyKinematicStateChange()
-	    self.dp = -self.dp
-	 end
-      end
-
-      self.connections:add(
-	 self.timer:sigTimeout():connect(self.onTimeout))
-
-      cnoid.MainWindow.instance():addToolBar(self.toolBar)
-
-      setmetatable(self, { 
-         __gc = function(self)
-	    print "cnoid.MainWindow.instance():removeToolBar(self.toolBar)"
-	    cnoid.MainWindow.instance():removeToolBar(self.toolBar)
-	 end
-      })
-
-      return self
-   end
+self = { 
+   connections = cnoid.ScopedConnectionSet.new()
 }
 
-bodyShaker = BodyShaker.new()
+local toolBar = cnoid.ToolBar.new("ShakeBar")
+toolBar:setVisibleByDefault(true)
+local button = toolBar:addToggleButton("Shake")
+button:setChecked(true)
+local bodyItems = { }
+local timer = cnoid.Timer.new()
+local dp = cnoid.Vector3(0.0, 0.0, 0.01)
+
+local onSelectionChanged = function(items)
+   bodyItems = cnoid.extractBodyItems(items)
+   if #bodyItems > 0 and button:isChecked() then
+      timer:start(50)
+   else
+      timer:stop()
+   end
+end
+   
+self.connections:add(
+   cnoid.ItemTreeView.instance():sigSelectionChanged():connect(onSelectionChanged))
+
+self.connections:add(
+   button:sigToggled():connect(
+      function(on)
+	 onSelectionChanged(cnoid.ItemTreeView.instance():selectedItems())
+      end))
+
+self.connections:add(
+   timer:sigTimeout():connect(
+      function()
+	 for i, bodyItem in ipairs(bodyItems) do
+	    local body = bodyItem:body()
+	    local rootLink = body:rootLink()
+	    rootLink:setTranslation(rootLink:translation() + dp)
+	    body:calcForwardKinematics()
+	    bodyItem:notifyKinematicStateChange()
+	    dp = -dp
+	 end
+      end))
+
+self.finalize = function(self)
+   self.connections:disconnect()
+   cnoid.MainWindow.instance():removeToolBar(toolBar)
+end
+
+setmetatable(self, { __gc = self.finalize })
+
+if bodyShaker then
+   bodyShaker:finalize()
+end
+
+cnoid.MainWindow.instance():addToolBar(toolBar)
+   
+bodyShaker = self
