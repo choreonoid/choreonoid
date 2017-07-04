047bb5bc40736436f7ae49474abac17cc871581a
Takuro Sasaki <takuro-sasaki@aist.go.jp>
2017-07-04 17:37:07 +0900

    replace undefined input of PS4 analog stick before initialization

--- src/Util/JoystickLinux.cpp
+++ src/Util/JoystickLinux.cpp
@@ -40,6 +40,10 @@ public:
     Model model;
     vector<int> axis_map;
     vector<int> button_map;
+    vector<bool> record_init_pos;
+    vector<double> initial_pos;
+    vector<bool> initialized;
+    vector<double> PS4_axes_default_pos;
 
     JoystickImpl(Joystick* self, const char* device);
     ~JoystickImpl();
@@ -124,6 +128,13 @@ bool JoystickImpl::openDevice(const char* device)
       model = PS4;
       axis_map = PS4_AM;
       button_map = PS4_BM;
+      record_init_pos.assign(axisEnabled.size(), false);
+      initial_pos.assign(axisEnabled.size(), 0.0);
+      initialized.assign(axisEnabled.size(), false);
+      vector<double> PS4_analog_input_defaults = { 0.0, 0.0, 0.0, -1.0, -1.0, 0.0 };  // Lstick_H, Lstick_V, Rstick_H, L2, R2, Rstick_V
+      vector<double> PS4_default_positions(axisEnabled.size()-PS4_analog_input_defaults.size(), 0.0);
+      copy(PS4_analog_input_defaults.begin(), PS4_analog_input_defaults.end(), back_inserter(PS4_axes_default_pos));
+      copy(PS4_default_positions.begin(), PS4_default_positions.end(), back_inserter(PS4_axes_default_pos));
     }else if(model_id == "Sony PLAYSTATION(R)3 Controller"){
       model = PS3;
       axis_map = PS3_AM;
@@ -261,6 +272,23 @@ bool JoystickImpl::readEvent()
         if(axisEnabled[id]){
             // normalize value (-1.0〜1.0)
             pos = nearbyint(pos * 10.0) / 10.0;
+            if(model==PS4){
+                // analog stick input of PS4 is -1.0 before first operation.
+                if(!record_init_pos[id]){
+                    initial_pos[id] = pos;
+                    pos = PS4_axes_default_pos[id];    // replace input for default value
+                    record_init_pos[id] = true;
+                    initialized[id] = false;
+                } else {
+                    if(!initialized[id]){
+                        if(pos == initial_pos[id]){
+                            pos = PS4_axes_default_pos[id];    // replace input for default value
+                        } else {
+                            initialized[id]=true;     // analog stick input changed
+                        }
+                    }
+                }
+            }
             double prevPos = axes[id];
             if(pos != prevPos){
                 axes[id] = pos;
