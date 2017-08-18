0fcf8f6e1808ea4cb1826644964d48fdc665a382
susaikum <ikumi.susa@vmc-motion.com>
2017-08-18 11:02:51 +0900

    Add files of AGXSubsimulator

---
+++ src/AGXDynamicsPlugin/AGXSubSimulator/AGXSubSimlatorPlugin.cpp
@@ -0,0 +1,22 @@
+#include "AGXSubSimulatorItem.h"
+#include <cnoid/Plugin>
+
+using namespace cnoid;
+
+class AGXSubSimulatorPlugin : public Plugin
+{
+public:
+
+    AGXSubSimulatorPlugin() : Plugin("AGXSubSimulator") {
+        require("Body");
+        require("AGXDynamics");
+    }
+
+    virtual bool initialize() {
+        AGXSubSimulatorItem::initializeClass(this);
+        return true;
+    }
+};
+
+
+CNOID_IMPLEMENT_PLUGIN_ENTRY(AGXSubSimulatorPlugin);
