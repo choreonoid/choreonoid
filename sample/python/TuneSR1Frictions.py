eea6acf86afb32d28732a12d6fd5c8ee13695347
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2017-10-19 21:45:01 +0900

    Remove a Python sample to set friction parameters. Material table can be used for the same purpose in the current version.

--- sample/python/TuneSR1Frictions.py
+++
@@ -1,9 +0,0 @@
-from cnoid.Base import *
-from cnoid.BodyPlugin import *
-
-sr1 = Item.find("SR1").body()
-floorLink = Item.find("Floor").body().rootLink()
-simulator = Item.find("AISTSimulator")
-
-simulator.setFriction(sr1.link("LLEG_ANKLE_R"), floorLink, 1.0, 1.0)
-simulator.setFriction(sr1.link("RLEG_ANKLE_R"), floorLink, 0.0, 0.0)
