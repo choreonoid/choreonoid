d4676ddd0eaa8fe054fa9b749b16aeceab2ca428
susaikum <ikumi.susa@vmc-motion.com>
2017-10-27 15:59:41 +0900

    Changed extract to read at loading material in YAMLBodyLoader.cpp

--- src/Body/YAMLBodyLoader.cpp
+++ src/Body/YAMLBodyLoader.cpp
@@ -818,7 +818,7 @@ LinkPtr YAMLBodyLoaderImpl::readLinkContents(Mapping* node)
 
     readJointContents(link, node);
 
-    if(node->extract("material", symbol)){
+    if(node->read("material", symbol)){
         link->setMaterial(symbol);
     }
     
