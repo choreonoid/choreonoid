041c6189c798f20eb6bc18dbda366b78c59d2cc9
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2017-11-27 11:07:31 +0900

    fix typo

--- .travis-osx-macports.sh
+++ .travis-osx-macports.sh
@@ -5,7 +5,7 @@ curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macp
 chmod +x ./macports-ci
 ./macports-ci install
 PATH="/opt/local/bin:$PATH"
-./misc/script/install-requisities-macports.sh
+./misc/script/install-requisites-macports.sh
 cmake .
 make
 sudo make install
