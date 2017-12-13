144c43000f0dd5ee4cba644de62137413490063c
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2017-12-13 17:47:06 +0900

    Add BodyCollisionDetector.h

---
+++ src/Body/BodyCollisionDetector.h
@@ -0,0 +1,36 @@
+/**
+   \file
+   \author Shin'ichiro Nakaoka
+*/
+
+#ifndef CNOID_BODY_BODY_COLLISION_DETECTOR_H
+#define CNOID_BODY_BODY_COLLISION_DETECTOR_H
+
+#include <cnoid/CollisionDetector>
+#include "exportdecl.h"
+
+namespace cnoid {
+
+class CNOID_EXPORT BodyCollisionDetector : public Referenced
+{
+public:
+    BodyCollisionDetector();
+    virtual ~BodyCollisionDetector();
+
+    void setCollisionDetecotr(CollisionDetector* collisionDetector);
+    CollisionDetector* collisionDetector();
+    
+    void addBody(Body* body, bool enableSelfCollisions = true);
+
+    bool makeReady();
+
+    void updatePositions();
+    void detectCollisions(std::function<void(Link* link1, Link* link2, const CollisionArray& collisions)> callback);
+
+    bool isFindClosestPointsAvailable() const;
+    double findClosestPoints(Link* link1, Link* link2, Vector3& out_point1, Vector3& out_point2);
+};
+
+}
+
+#endif
