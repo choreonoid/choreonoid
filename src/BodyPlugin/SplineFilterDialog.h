780caa1f18223a1e0fbe07d49677d73aebc91576
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2017-12-14 11:59:21 +0900

    Add a filiter to change the frame rate of an motion trajectory item

---
+++ src/BodyPlugin/SplineFilterDialog.h
@@ -0,0 +1,16 @@
+/**
+   @author Shin'ichiro Nakaoka
+*/
+
+#ifndef CNOID_BODY_PLUGIN_SIPLINE_FILETER_DIALOG_H
+#define CNOID_BODY_PLUGIN_SIPLINE_FILETER_DIALOG_H
+
+namespace cnoid {
+    
+class ExtensionManager;
+
+void initializeSplineFilterDialog(ExtensionManager* ext);
+
+}
+
+#endif
