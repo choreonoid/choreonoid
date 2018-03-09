041bbc2a1e916ea1afe6ec09807dd8c4f52aa111
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2018-03-09 23:24:44 +0900

    Arrange sample WRS2018 projects written in Python

---
+++ sample/WRS2018/T1+T4-AizuSpiderSS.py
@@ -0,0 +1,2 @@
+import WRSUtil
+WRSUtil.loadProject("T1+T4", "AISTSimulator", "AizuSpiderSS")
---
+++ sample/WRS2018/WRSUtil.py
@@ -0,0 +1,18 @@
+import os
+import cnoid.Base
+
+def loadProject(worldProject, simulatorProject, robotProject):
+
+    directory = os.path.dirname(os.path.realpath(__file__))
+    
+    pm = cnoid.Base.ProjectManager.instance()
+
+    pm.loadProject(os.path.join(directory, worldProject + ".cnoid"))
+
+    worldItem = cnoid.Base.Item.find("World")
+
+    pm.loadProject(os.path.join(directory, simulatorProject + ".cnoid"), worldItem)
+
+    pm.loadProject(os.path.join(directory, robotProject + ".cnoid"), worldItem)
+
+    pm.setCurrentProjectName(worldProject + "-" + robotProject)
