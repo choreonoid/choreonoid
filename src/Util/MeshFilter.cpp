4703297b01cbedd05084220803d7032b33414056
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2018-05-05 01:54:36 +0900

    Implementing MeshFilter::removeRedundantFaces

--- src/Util/MeshFilter.cpp
+++ src/Util/MeshFilter.cpp
@@ -6,8 +6,10 @@
 #include "MeshFilter.h"
 #include "MeshExtractor.h"
 #include "SceneDrawables.h"
-#include <unordered_set>
+#include "IdPair.h"
+#include <unordered_map>
 #include <functional>
+#include <array>
 
 using namespace std;
 using namespace cnoid;
@@ -16,47 +18,77 @@ namespace {
 
 const float PI = 3.14159265358979323846f;
 
+typedef array<int, 3> FaceId;
+
+template<class Triangle>
+FaceId faceId(Triangle& triangle)
+{
+    FaceId id = { triangle[0], triangle[1], triangle[2] };
+    std::sort(id.begin(), id.end());
+    return id;
+}
+
+typedef vector<int> FaceSet; // Set of triangle indices
+typedef unordered_map<FaceId, FaceSet> IdenticalFaceSetMap;
+
+typedef IdPair<int> EdgeId;
+
+EdgeId edgeId(SgMesh::TriangleRef triangle, int index)
+{
+    static const int offsets[][2] = { { 0, 1 }, { 1, 2 }, { 2, 0 } };
+    return EdgeId(triangle[offsets[index][0]], triangle[offsets[index][1]]);
+}
+
 }
 
 namespace std {
 
-template<> struct hash<Array3i>
+template<> struct hash<FaceId>
 {
     void hash_combine(std::size_t& seed, int v) const {
         std::hash<int> hasher;
         seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
     }
-    std::size_t operator()(const Array3i& value) const {
+    std::size_t operator()(const FaceId& id) const {
         std::size_t seed = 0;
-        hash_combine(seed, value[0]);
-        hash_combine(seed, value[1]);
-        hash_combine(seed, value[2]);
+        hash_combine(seed, id[0]);
+        hash_combine(seed, id[1]);
+        hash_combine(seed, id[2]);
         return seed;
     }
 };
 
 }
 
-
 namespace cnoid {
 
 class MeshFilterImpl
 {
 public:
+    unique_ptr<MeshExtractor> meshExtractor;
+    vector<Vector3f> faceNormals;
+    vector<FaceSet> facesOfVertexMap;
+    vector<vector<int>> normalsOfVertexMap;
+
+    SgIndexArray orgTriangles;
+    IdenticalFaceSetMap identicalFaceSetMap;
+    unordered_map<EdgeId, FaceSet> facesOfEdgeMap;
+    
     float minCreaseAngle;
     float maxCreaseAngle;
-    SgNormalArrayPtr faceNormals;
-    vector<vector<int>> vertexIndexToTriangleIndicesMap;
-    vector<vector<int>> vertexIndexToNormalIndicesMap;
-    unique_ptr<MeshExtractor> meshExtractor;
     bool isNormalOverwritingEnabled;
 
     MeshFilterImpl();
     MeshFilterImpl(const MeshFilterImpl& org);
     void forAllMeshes(SgNode* node, function<void(SgMesh* mesh)> callback);
     void removeRedundantVertices(SgMesh* mesh);
+    void determineFaceSideToRemain(SgMesh* mesh, const SgIndexArray& orgTriangles);
     void removeRedundantFaces(SgMesh* mesh);
+    void determineFaceSideToRemain(SgMesh* mesh);
+    void determineFaceSideToRemainIter(SgMesh* mesh, vector<bool>& traversed, int faceIndex, int prevFaceIndex);
+    void makeFacesOfEdgeMap(SgMesh* mesh);
     void calculateFaceNormals(SgMesh* mesh);
+    void makeFacesOfVertexMap(SgMesh* mesh, bool removeSameNormalFaces = false);
     void setVertexNormals(SgMesh* mesh, float creaseAngle);
 };
 
@@ -139,13 +171,11 @@ bool MeshFilter::generateNormals(SgMesh* mesh, float creaseAngle, bool removeRed
         return false;
     }
 
-    if(!impl->faceNormals){
-        impl->faceNormals = new SgNormalArray;
-    }
     if(removeRedundantVertices){
         impl->removeRedundantVertices(mesh);
     }
     impl->calculateFaceNormals(mesh);
+    impl->makeFacesOfVertexMap(mesh, true);
     impl->setVertexNormals(mesh, creaseAngle);
 
     return true;
@@ -229,41 +259,98 @@ void MeshFilter::removeRedundantFaces(SgMesh* mesh)
 
 void MeshFilterImpl::removeRedundantFaces(SgMesh* mesh)
 {
-    unordered_set<Array3i> existingFaces;
+    const int numOrgTriangles = mesh->numTriangles();
+    if(numOrgTriangles == 0){
+        return;
+    }
 
-    const auto orgTriangles = mesh->triangleVertices();
+    calculateFaceNormals(mesh);
+
+    orgTriangles = mesh->triangleVertices();
+    identicalFaceSetMap.clear();
+    identicalFaceSetMap.reserve(numOrgTriangles);
     auto& triangles = mesh->triangleVertices();
     triangles.clear();
-    for(size_t i=0; i < orgTriangles.size(); ++i){
-        Array3i triangle(orgTriangles[i * 3]);
-
-        // sort triangle elements
-
-        /*
-        auto result = existingFaces.insert(triangle);
-        if(result.second){
+    
+    for(int i=0; i < numOrgTriangles; ++i){
+        SgMesh::ConstTriangleRef triangle(&orgTriangles[i*3]);
+        auto inserted = identicalFaceSetMap.insert(make_pair(faceId(triangle), FaceSet()));
+        if(inserted.second){
             mesh->newTriangle() = triangle;
         }
-        */
+        auto iter = inserted.first;
+        iter->second.push_back(i);
+    }
+
+    if(mesh->numTriangles() != numOrgTriangles){
+        determineFaceSideToRemain(mesh);
     }
 }
 
+
+void MeshFilterImpl::determineFaceSideToRemain(SgMesh* mesh)
+{
+    makeFacesOfEdgeMap(mesh);
+    vector<bool> traversed(mesh->numTriangles(), false);
+    determineFaceSideToRemainIter(mesh, traversed, 0, 0);
+}
+
+
+void MeshFilterImpl::determineFaceSideToRemainIter
+(SgMesh* mesh, vector<bool>& traversed, int faceIndex, int prevFaceIndex)
+{
+    if(traversed[faceIndex]){
+        return;
+    }
+    traversed[faceIndex] = true;
+
+    auto triangle = mesh->triangle(faceIndex);
+    auto& faces = identicalFaceSetMap[faceId(triangle)];
+    
+    if(faces.size() >= 2){
+        if(faceIndex == prevFaceIndex){
+            faces.resize(1);
+        } else {
+
+        }
+    }
+
+    for(int i=0; i < 3; ++i){
+        auto& facesOfEdge = facesOfEdgeMap[edgeId(triangle, i)];
+        for(auto& adjacentFaceIndex : facesOfEdge){
+            if(adjacentFaceIndex != faceIndex){
+                determineFaceSideToRemainIter(
+                    mesh, traversed, adjacentFaceIndex, faceIndex);
+            }
+        }
+    }
+}
+
+
+void MeshFilterImpl::makeFacesOfEdgeMap(SgMesh* mesh)
+{
+    facesOfEdgeMap.clear();
+    facesOfEdgeMap.reserve(mesh->vertices()->size());
+    const int numTriangles = mesh->numTriangles();
+    for(int i=0; i < numTriangles; ++i){
+        SgMesh::TriangleRef triangle = mesh->triangle(i);
+        for(int j=0; j < 3; ++j){
+            facesOfEdgeMap[edgeId(triangle, j)].push_back(i);
+        }
+    }
+}
+    
 
 void MeshFilterImpl::calculateFaceNormals(SgMesh* mesh)
 {
     const SgVertexArray& vertices = *mesh->vertices();
     const int numVertices = vertices.size();
     const int numTriangles = mesh->numTriangles();
-
-    faceNormals->clear();
-    faceNormals->reserve(numTriangles);
-    vertexIndexToTriangleIndicesMap.clear();
-    vertexIndexToTriangleIndicesMap.resize(numVertices);
+    faceNormals.clear();
+    faceNormals.reserve(numTriangles);
 
     for(int i=0; i < numTriangles; ++i){
-
         SgMesh::TriangleRef triangle = mesh->triangle(i);
-        
         const Vector3f& v0 = vertices[triangle[0]];
         const Vector3f& v1 = vertices[triangle[1]];
         const Vector3f& v2 = vertices[triangle[2]];
@@ -274,26 +361,41 @@ void MeshFilterImpl::calculateFaceNormals(SgMesh* mesh)
         }else{
           normal.normalize();
         }
-        faceNormals->push_back(normal);
+        faceNormals.push_back(normal);
+    }
+}
 
+
+void MeshFilterImpl::makeFacesOfVertexMap(SgMesh* mesh, bool removeSameNormalFaces)
+{
+    facesOfVertexMap.clear();
+    facesOfVertexMap.resize(mesh->vertices()->size());
+    const int numTriangles = mesh->numTriangles();
+    
+    for(int i=0; i < numTriangles; ++i){
+        SgMesh::TriangleRef triangle = mesh->triangle(i);
         for(int j=0; j < 3; ++j){
-            vector<int>& trianglesOfVertex = vertexIndexToTriangleIndicesMap[triangle[j]];
-
-            /**
-               \todo Angle between adjacent edges should be taken into account
-               to generate natural normals
-            */
-            bool isSameNormalFaceFound = false;
-            for(size_t k=0; k < trianglesOfVertex.size(); ++k){
-                const Vector3f& otherNormal = (*faceNormals)[trianglesOfVertex[k]];
-                // the same face is not appended
-                if(otherNormal.isApprox(normal, 5.0e-4)){
-                    isSameNormalFaceFound = true;
-                    break;
+            auto& faceIndicesOfVertex = facesOfVertexMap[triangle[j]];
+            if(!removeSameNormalFaces){
+                faceIndicesOfVertex.push_back(i);
+            } else {
+                /**
+                   \todo Angle between adjacent edges should be taken into account
+                   to generate natural normals
+                */
+                const auto& normal = faceNormals[i];
+                bool isSameNormalFaceFound = false;
+                for(size_t k=0; k < faceIndicesOfVertex.size(); ++k){
+                    const auto& adjacentFaceNormal = faceNormals[faceIndicesOfVertex[k]];
+                    // the same face is not appended
+                    if(adjacentFaceNormal.isApprox(normal, 5.0e-4)){
+                        isSameNormalFaceFound = true;
+                        break;
+                    }
                 }
-            }
-            if(!isSameNormalFaceFound){
-                trianglesOfVertex.push_back(i);
+                if(!isSameNormalFaceFound){
+                    faceIndicesOfVertex.push_back(i);
+                }
             }
         }
     }
@@ -304,8 +406,7 @@ void MeshFilterImpl::setVertexNormals(SgMesh* mesh, float givenCreaseAngle)
 {
     const float creaseAngle = std::max(minCreaseAngle, std::min(maxCreaseAngle, givenCreaseAngle));
     
-    const SgVertexArray& vertices = *mesh->vertices();
-    const int numVertices = vertices.size();
+    const int numVertices = mesh->vertices()->size();
     const int numTriangles = mesh->numTriangles();
 
     mesh->setNormals(new SgNormalArray());
@@ -314,8 +415,8 @@ void MeshFilterImpl::setVertexNormals(SgMesh* mesh, float givenCreaseAngle)
     normalIndices.clear();
     normalIndices.reserve(mesh->triangleVertices().size());
 
-    vertexIndexToNormalIndicesMap.clear();
-    vertexIndexToNormalIndicesMap.resize(numVertices);
+    normalsOfVertexMap.clear();
+    normalsOfVertexMap.resize(numVertices);
 
     for(int faceIndex=0; faceIndex < numTriangles; ++faceIndex){
 
@@ -324,15 +425,15 @@ void MeshFilterImpl::setVertexNormals(SgMesh* mesh, float givenCreaseAngle)
         for(int i=0; i < 3; ++i){
 
             const int vertexIndex = triangle[i];
-            const vector<int>& trianglesOfVertex = vertexIndexToTriangleIndicesMap[vertexIndex];
-            const Vector3f& currentFaceNormal = (*faceNormals)[faceIndex];
+            const auto& faceIndicesOfVertex = facesOfVertexMap[vertexIndex];
+            const Vector3f& currentFaceNormal = faceNormals[faceIndex];
             Vector3f normal = currentFaceNormal;
             bool normalIsFaceNormal = true;
                 
             // avarage normals of the faces whose crease angle is below the 'creaseAngle' variable
-            for(size_t j=0; j < trianglesOfVertex.size(); ++j){
-                const int adjacentFaceIndex = trianglesOfVertex[j];
-                const Vector3f& adjacentFaceNormal = (*faceNormals)[adjacentFaceIndex];
+            for(size_t j=0; j < faceIndicesOfVertex.size(); ++j){
+                const int adjacentFaceIndex = faceIndicesOfVertex[j];
+                const Vector3f& adjacentFaceNormal = faceNormals[adjacentFaceIndex];
                 float cosAngle = currentFaceNormal.dot(adjacentFaceNormal)
                   / (currentFaceNormal.norm() * adjacentFaceNormal.norm());
                 //prevent NaN
@@ -352,7 +453,7 @@ void MeshFilterImpl::setVertexNormals(SgMesh* mesh, float givenCreaseAngle)
             
             for(int j=0; j < 3; ++j){
                 const int vertexIndex2 = triangle[j];
-                const vector<int>& normalIndicesOfVertex = vertexIndexToNormalIndicesMap[vertexIndex2];
+                const vector<int>& normalIndicesOfVertex = normalsOfVertexMap[vertexIndex2];
                 for(size_t k=0; k < normalIndicesOfVertex.size(); ++k){
                     int index = normalIndicesOfVertex[k];
                     if(normals[index].isApprox(normal)){
@@ -364,7 +465,7 @@ void MeshFilterImpl::setVertexNormals(SgMesh* mesh, float givenCreaseAngle)
             if(normalIndex < 0){
                 normalIndex = normals.size();
                 normals.push_back(normal);
-                vertexIndexToNormalIndicesMap[vertexIndex].push_back(normalIndex);
+                normalsOfVertexMap[vertexIndex].push_back(normalIndex);
             }
             
     normalIndexFound:
