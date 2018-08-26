65e0c13e34c517d1f7a5ee860aeb5d5932446145
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2018-08-27 02:38:26 +0900

    Reimplement SimulationPeriodicExecutionContext

--- src/OpenRTMPlugin/SimulationPeriodicExecutionContext.cpp
+++ src/OpenRTMPlugin/SimulationPeriodicExecutionContext.cpp
@@ -1,177 +1,27 @@
 /*!
   \file SimulationPeriodicExecutionContext.cpp
-  \author Shizuko Hattori
   \author Shin'ichiro Nakaoka
 */
 
 #include "SimulationPeriodicExecutionContext.h"
-#include "LoggerUtil.h"
-#include <rtm/ECFactory.h>
-
-#if defined(OPENRTM_VERSION12)
-#include <rtm/RTObjectStateMachine.h>
-#endif
 
 using namespace cnoid;
 
-SimulationPeriodicExecutionContext::SimulationPeriodicExecutionContext()
-    : PeriodicExecutionContext()
-{
-}
-
-
-SimulationPeriodicExecutionContext::~SimulationPeriodicExecutionContext()
-{
-
-}
-
-
-RTC::ReturnCode_t SimulationPeriodicExecutionContext::activate_component(RTC::LightweightRTObject_ptr comp)
-throw (CORBA::SystemException)
-{
-    DDEBUG("SimulationPeriodicExecutionContext::activate_component");
-
-#if defined(OPENRTM_VERSION11)
-
-    CompItr it = std::find_if(m_comps.begin(), m_comps.end(), find_comp(comp));
-    if (it == m_comps.end()) {
-        return RTC::BAD_PARAMETER;
-    }
-
-    if (!(it->_sm.m_sm.isIn(RTC::INACTIVE_STATE))) {
-        return RTC::PRECONDITION_NOT_MET;
-    }
-
-    it->_sm.m_sm.goTo(RTC::ACTIVE_STATE);
-
-    it->_sm.worker();
-
-    if (it->_sm.m_sm.isIn(RTC::ACTIVE_STATE)) {
-        return RTC::RTC_OK;
-    }
-
-    return RTC::RTC_ERROR;
-
-#elif defined(OPENRTM_VERSION12)
-
-    RTC_impl::RTObjectStateMachine* rtobj = m_worker.findComponent(comp);
-
-    if (rtobj == NULL) {
-        return RTC::BAD_PARAMETER;
-    }
-    if (!(rtobj->isCurrentState(RTC::INACTIVE_STATE))) {
-        return RTC::PRECONDITION_NOT_MET;
-    }
-    m_syncActivation = false;
-
-    RTC::ReturnCode_t ret = ExecutionContextBase::activateComponent(comp);
-    invokeWorkerPreDo();
-    if (rtobj->isCurrentState(RTC::ACTIVE_STATE)) {
-        return RTC::RTC_OK;
-    }
-    return RTC::RTC_ERROR;
-
-#endif
-}
-
+#ifdef OPENRTM_VERSION11
 
 RTC::ReturnCode_t SimulationPeriodicExecutionContext::deactivate_component(RTC::LightweightRTObject_ptr comp)
 throw (CORBA::SystemException)
 {
-    DDEBUG("SimulationPeriodicExecutionContext::deactivate_component");
     RTC_TRACE(("deactivate_component()"));
-
-#if defined(OPENRTM_VERSION11)
-
+    
     CompItr it = std::find_if(m_comps.begin(), m_comps.end(), find_comp(comp));
-    if (it == m_comps.end()) {
-        DDEBUG("SimulationPeriodicExecutionContext::deactivate_component BAD_PARAMETER");
-        return RTC::BAD_PARAMETER;
-    }
-    if (!(it->_sm.m_sm.isIn(RTC::ACTIVE_STATE))) {
-        DDEBUG("SimulationPeriodicExecutionContext::deactivate_component PRECONDITION_NOT_MET");
+    if(it == m_comps.end()) { return RTC::BAD_PARAMETER; }
+    if(!(it->_sm.m_sm.isIn(RTC::ACTIVE_STATE))){
         return RTC::PRECONDITION_NOT_MET;
     }
-
     it->_sm.m_sm.goTo(RTC::INACTIVE_STATE);
-
-    it->_sm.worker();
-
-    if (it->_sm.m_sm.isIn(RTC::INACTIVE_STATE)) {
-        RTC_TRACE(("The component has been properly deactivated."));
-        DDEBUG("SimulationPeriodicExecutionContext::deactivate_component RTC_OK");
-        return RTC::RTC_OK;
-    }
-
-    RTC_ERROR(("The component could not be deactivated."));
-    DDEBUG("SimulationPeriodicExecutionContext::deactivate_component RTC_ERROR");
-    return RTC::RTC_ERROR;
-
-#elif defined(OPENRTM_VERSION12)
-
-    RTC_impl::RTObjectStateMachine* rtobj = m_worker.findComponent(comp);
-
-    if (rtobj == NULL) {
-        return RTC::BAD_PARAMETER;
-    }
-    if (!(rtobj->isCurrentState(RTC::ACTIVE_STATE))) {
-        return RTC::PRECONDITION_NOT_MET;
-    }
-
-    m_syncDeactivation = false;
-    RTC::ReturnCode_t ret = ExecutionContextBase::deactivateComponent(comp);
-    invokeWorkerPreDo();
-
-    if (rtobj->isCurrentState(RTC::INACTIVE_STATE)) {
-        return RTC::RTC_OK;
-    }
-
-    return RTC::RTC_ERROR;
-
-#endif
+    
+    return RTC::RTC_OK;
 }
 
-
-RTC::ReturnCode_t SimulationPeriodicExecutionContext::reset_component(RTC::LightweightRTObject_ptr comp)
-throw (CORBA::SystemException)
-{
-#if defined(OPENRTM_VERSION11)
-    CompItr it = std::find_if(m_comps.begin(), m_comps.end(), find_comp(comp));
-    if (it == m_comps.end()) {
-        return RTC::BAD_PARAMETER;
-    }
-
-    if (!(it->_sm.m_sm.isIn(RTC::ERROR_STATE))) {
-        return RTC::PRECONDITION_NOT_MET;
-    }
-
-    it->_sm.m_sm.goTo(RTC::INACTIVE_STATE);
-    it->_sm.worker();
-
-    if (it->_sm.m_sm.isIn(RTC::INACTIVE_STATE)) {
-        return RTC::RTC_OK;
-    }
-
-    return RTC::RTC_ERROR;
-
-#elif defined(OPENRTM_VERSION12)
-
-    RTC_impl::RTObjectStateMachine* rtobj = m_worker.findComponent(comp);
-
-    if (rtobj == NULL) {
-        return RTC::BAD_PARAMETER;
-    }
-    if (!(rtobj->isCurrentState(RTC::ERROR_STATE))) {
-        return RTC::PRECONDITION_NOT_MET;
-    }
-    m_syncReset = false;
-    RTC::ReturnCode_t ret = ExecutionContextBase::resetComponent(comp);
-    invokeWorkerPreDo();
-
-    if (rtobj->isCurrentState(RTC::INACTIVE_STATE)) {
-        return RTC::RTC_OK;
-    }
-    return RTC::RTC_ERROR;
-
 #endif
-}
--- src/OpenRTMPlugin/SimulationPeriodicExecutionContext.h
+++ src/OpenRTMPlugin/SimulationPeriodicExecutionContext.h
@@ -1,15 +1,11 @@
 /*!
   \file SimulationPeriodicExecutionContext.h
-  \author Shizuko Hattori
   \author Shin'ichiro Nakaoka
 */
 
 #ifndef CNOID_OPENRTM_PLUGIN_SIMULATION_PERIODIC_EXECUTION_CONTEXT_H
 #define CNOID_OPENRTM_PLUGIN_SIMULATION_PERIODIC_EXECUTION_CONTEXT_H
 
-#include <rtm/RTC.h>
-#include <coil/Task.h>
-#include <rtm/Manager.h>
 #include <rtm/PeriodicExecutionContext.h>
 
 #ifdef WIN32
@@ -17,25 +13,20 @@
 #endif
 
 namespace cnoid {
-/*!
-  To call 'tick()' when the 'deactivate_component" function is called,
-  OpenHRPExecutionContext is redefined as this class in the OpenRTM plugin.
-  See post 02356 to the openrtm-users mailing list.
-*/
+
 #ifdef OPENRTM_VERSION11
 class SimulationPeriodicExecutionContext : public virtual RTC::PeriodicExecutionContext
-#elif defined(OPENRTM_VERSION12)
-class SimulationPeriodicExecutionContext : public virtual RTC_exp::PeriodicExecutionContext
-#endif
 {
 public:
-    SimulationPeriodicExecutionContext();
-    virtual ~SimulationPeriodicExecutionContext(void);
-
-    virtual RTC::ReturnCode_t activate_component(RTC::LightweightRTObject_ptr comp) throw (CORBA::SystemException);
     virtual RTC::ReturnCode_t deactivate_component(RTC::LightweightRTObject_ptr comp) throw (CORBA::SystemException);
-    virtual RTC::ReturnCode_t reset_component(RTC::LightweightRTObject_ptr comp) throw (CORBA::SystemException);
+    
 };
+
+#elif defined(OPENRTM_VERSION12)
+// Temporary
+typedef RTC_exp::PeriodicExecutionContext SimulationPeriodicExecutionContext;
+
+#endif
 
 }
 
