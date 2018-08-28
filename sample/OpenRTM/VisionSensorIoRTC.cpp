63f75569784a217c0ec8e8765e3543d8215da18e
hattori <s.hattori@aist.go.jp>
2018-08-28 13:14:47 +0900

    Fixed memory leak

--- sample/OpenRTM/VisionSensorIoRTC.cpp
+++ sample/OpenRTM/VisionSensorIoRTC.cpp
@@ -340,6 +340,9 @@ void CameraIo::outputImage()
         void* dis = (void*)cameraImage.data.image.raw_data.get_buffer();
         memcpy(dis, outbuffer, writtenSize);
 
+        free(row_pointers);
+        free(outbuffer);
+
     }else{
 
         switch(camera->imageType()){
