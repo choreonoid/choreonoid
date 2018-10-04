05f7112ba39e755715635c28937e05b716ef04be
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2018-10-05 01:06:43 +0900

    Tune the door destruction of T3

--- sample/WRS2018/src/CarT3Controller.cpp
+++ sample/WRS2018/src/CarT3Controller.cpp
@@ -13,17 +13,21 @@ using namespace cnoid;
 class CarT3Controller : public SimpleController
 {
     struct SpreaderTarget {
+        Link* link;
         ForceSensorPtr forceSensor;
         DevicePtr breakableJoint;
-        double time;
+        double timeToBreak;
+        int forceIteration;
+        double forceDuration;
+        double force;
         string sensorName;
         string jointName;
         SpreaderTarget(const char* sensorName, const char* jointName)
-            : time(0.0), sensorName(sensorName), jointName(jointName) { }
+            : timeToBreak(0.0), forceIteration(0), sensorName(sensorName), jointName(jointName) { }
     };
     vector<SpreaderTarget> targets;
     SimpleControllerIO* io;
-    double dt;
+    double timeStep;
     SpreaderController* spreaderController;
     
 public:
@@ -31,7 +35,7 @@ public:
     virtual bool initialize(SimpleControllerIO* io) override
     {
         this->io = io;
-        dt = io->timeStep();
+        timeStep = io->timeStep();
         
         targets = {
             { "FR_DOOR_UPPER_HINGE_FORCE_SENSOR", "FR_DOOR_UPPER_HINGE_CONSTRAINT" },
@@ -46,7 +50,9 @@ public:
             target.forceSensor = body->findDevice<ForceSensor>(target.sensorName);
             target.breakableJoint = body->findDevice(target.jointName);
             if(target.forceSensor && target.breakableJoint){
+                target.link = target.forceSensor->link();
                 io->enableInput(target.forceSensor);
+                io->enableOutput(target.link, LINK_FORCE);
                 ++iter;
             } else {
                 iter = targets.erase(iter);
@@ -73,22 +79,36 @@ public:
     {
         bool doRequestToSpread = false;
         
-        auto iter = targets.begin();
-        while(iter != targets.end()){
-            auto& target = *iter;
+        for(auto& target : targets){
+            target.link->F_ext().setZero();
             double fy = target.forceSensor->f().y();
             if(fy > 100.0){
                 doRequestToSpread = true;
                 if(target.breakableJoint->on()){
-                    target.time += dt;
-                    //io->os() << target.forceSensor->name() << "'s time counter is " << target.time << endl;
-                    if(target.time > 3.0){
+                    target.timeToBreak += timeStep;
+                    //io->os() << target.forceSensor->name() << "'s time counter is " << target.timeToBreak << endl;
+                    if(target.timeToBreak > 1.5){
                         target.breakableJoint->on(false);
                         target.breakableJoint->notifyStateChange();
+                        target.forceIteration = 8;
+                        target.forceDuration = 0.1;
+                        target.force = 1000.0;
                     }
                 }
             }
-            ++iter;
+            /*
+            if(target.forceIteration > 0){
+                io->os() << target.link->name() << ": force applied" << endl;
+                Vector3 f = target.link->T() * Vector3(target.force, 0.0, 0.0);
+                target.link->addExternalForce(f, Vector3(-0.72, -0.02, 0.3));
+                target.forceDuration -= timeStep;
+                if(target.forceDuration <= 0.0){
+                    target.forceDuration = 0.1;
+                    target.force = -target.force;
+                    --target.forceIteration;
+                }
+            }
+            */
         }
 
         if(spreaderController){
