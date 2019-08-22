3186ea37940c48b12b561dd4d5d4503e697d09e2
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2019-08-22 18:35:57 +0900

    Add the empty implementation of the ManipulatorPosition class

---
+++ src/Body/ManipulatorPosition.cpp
@@ -0,0 +1,5 @@
+#include "ManipulatorPosition.h"
+
+using namespace std;
+using namespace cnoid;
+
---
+++ src/Body/ManipulatorPosition.h
@@ -0,0 +1,19 @@
+#ifndef CNOID_BODY_PLUGIN_MANIPULATOR_POSITION_H
+#define CNOID_BODY_PLUGIN_MANIPULATOR_POSITION_H
+
+#include <cnoid/Referenced>
+
+namespace cnoid {
+
+class CNOID_EXPORT ManipulatorPosition : public Referenced
+{
+public:
+
+
+};
+
+typedef ref_ptr<ManipulatorPosition> ManipulatorPositionPtr;
+
+}
+
+#endif
