3ee00b6a8cec0d66b6e60bd32ec89857a2045967
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2019-08-23 01:41:56 +0900

    Implement the ManipulatorPosition classes

--- src/Body/ManipulatorPosition.cpp
+++ src/Body/ManipulatorPosition.cpp
@@ -1,5 +1,133 @@
 #include "ManipulatorPosition.h"
+#include <cnoid/Body>
 
 using namespace std;
 using namespace cnoid;
 
+
+ManipulatorPosition::ManipulatorPosition(PositionType type)
+    : positionType_(type)
+{
+
+}
+    
+
+ManipulatorPosition::ManipulatorPosition(const ManipulatorPosition& org)
+    : positionType_(org.positionType_),
+      name_(org.name_)
+{
+
+}
+
+
+ManipulatorIkPosition* ManipulatorPosition::ikPosition()
+{
+    if(positionType_ == IK){
+        return static_cast<ManipulatorIkPosition*>(this);
+    }
+    return nullptr;
+}
+
+
+ManipulatorFkPosition* ManipulatorPosition::fkPosition()
+{
+    if(positionType_ == FK){
+        return static_cast<ManipulatorFkPosition*>(this);
+    }
+    return nullptr;
+}
+
+
+ManipulatorIkPosition::ManipulatorIkPosition()
+    : ManipulatorPosition(IK)
+{
+    baseFrame = 0;
+    toolFrame = 0;
+    configuration = 0;
+    phase.fill(0);
+}
+
+
+ManipulatorIkPosition::ManipulatorIkPosition(const ManipulatorIkPosition& org)
+    : ManipulatorPosition(org)
+{
+    translation = org.translation;
+    rpy = org.rpy;
+    baseFrame = org.baseFrame;
+    toolFrame = org.toolFrame;
+    configuration = org.configuration;
+    phase = org.phase;
+}
+    
+    
+ManipulatorIkPosition& ManipulatorIkPosition::operator=(const ManipulatorIkPosition& rhs)
+{
+    setName(rhs.name());
+    
+    translation = rhs.translation;
+    rpy = rhs.rpy;
+    baseFrame = rhs.baseFrame;
+    toolFrame = rhs.toolFrame;
+    configuration = rhs.configuration;
+    phase = rhs.phase;
+
+    return *this;
+}
+    
+
+ManipulatorPosition* ManipulatorIkPosition::clone()
+{
+    return new ManipulatorIkPosition(*this);
+}
+
+
+void ManipulatorIkPosition::extract(Body* body)
+{
+
+}
+
+
+bool ManipulatorIkPosition::apply(Body* body) const
+{
+    return false;
+}
+
+
+ManipulatorFkPosition::ManipulatorFkPosition()
+    : ManipulatorPosition(FK)
+{
+    q.fill(0.0);
+}
+
+
+ManipulatorFkPosition::ManipulatorFkPosition(const ManipulatorFkPosition& org)
+    : ManipulatorPosition(org)
+{
+    q = org.q;
+}
+
+
+ManipulatorFkPosition& ManipulatorFkPosition::operator=(const ManipulatorFkPosition& rhs)
+{
+    setName(rhs.name());
+    q = rhs.q;
+    return *this;
+}
+
+
+ManipulatorPosition* ManipulatorFkPosition::clone()
+{
+    return new ManipulatorFkPosition(*this);
+}
+
+
+void ManipulatorFkPosition::extract(Body* body)
+{
+
+}
+
+
+bool ManipulatorFkPosition::apply(Body* body) const
+{
+    return false;
+}
--- src/Body/ManipulatorPosition.h
+++ src/Body/ManipulatorPosition.h
@@ -2,17 +2,87 @@
 #define CNOID_BODY_PLUGIN_MANIPULATOR_POSITION_H
 
 #include <cnoid/Referenced>
+#include <cnoid/EigenTypes>
+#include <string>
+#include <array>
+#include "exportdecl.h"
 
 namespace cnoid {
+
+class ManipulatorIkPosition;
+class ManipulatorFkPosition;
+class Body;
 
 class CNOID_EXPORT ManipulatorPosition : public Referenced
 {
 public:
+    static constexpr int MAX_NUM_JOINTS = 7;
+    enum PositionType { IK, FK };
 
+    virtual ManipulatorPosition* clone() = 0;
 
+    int positionType() const { return positionType_; }
+    bool isIK() const { return (positionType_ == IK); };
+    bool isFK() const { return (positionType_ == FK); };
+
+    ManipulatorIkPosition* ikPosition();
+    ManipulatorFkPosition* fkPosition();
+
+    const std::string& name() const { return name_; };
+    void setName(const std::string& name){ name_ = name; }
+
+    virtual void extract(Body* body) = 0;
+    virtual bool apply(Body* body) const = 0;
+
+protected:
+    ManipulatorPosition(PositionType type);
+    ManipulatorPosition(const ManipulatorPosition& org);
+
+private:
+    PositionType positionType_;
+    std::string name_;
 };
 
 typedef ref_ptr<ManipulatorPosition> ManipulatorPositionPtr;
+
+
+class CNOID_EXPORT ManipulatorIkPosition : public ManipulatorPosition
+{
+public:
+    Vector3 translation;
+    Vector3 rpy;
+    int baseFrame;
+    int toolFrame;
+    int configuration;
+    std::array<int, MAX_NUM_JOINTS> phase;
+
+    ManipulatorIkPosition();
+    ManipulatorIkPosition(const ManipulatorIkPosition& org);
+    ManipulatorIkPosition& operator=(const ManipulatorIkPosition& rhs);
+
+    virtual ManipulatorPosition* clone() override;
+    virtual void extract(Body* body) override;
+    virtual bool apply(Body* body) const override;
+};
+
+typedef ref_ptr<ManipulatorIkPosition> ManipulatorIkPositionPtr;
+
+
+class CNOID_EXPORT ManipulatorFkPosition : public ManipulatorPosition
+{
+public:
+    std::array<double, MAX_NUM_JOINTS> q;
+
+    ManipulatorFkPosition();
+    ManipulatorFkPosition(const ManipulatorFkPosition& org);
+    ManipulatorFkPosition& operator=(const ManipulatorFkPosition& rhs);
+
+    virtual ManipulatorPosition* clone() override;
+    virtual void extract(Body* body) override;
+    virtual bool apply(Body* body) const override;
+};
+
+typedef ref_ptr<ManipulatorFkPosition> ManipulatorFkPositionPtr;
 
 }
 
