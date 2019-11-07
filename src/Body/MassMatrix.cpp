5859bb64d38e626d185d3b1afb869054bc68bb9c
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2019-11-07 18:18:14 +0900

    Minor fix

--- src/Body/MassMatrix.cpp
+++ src/Body/MassMatrix.cpp
@@ -74,9 +74,9 @@ void calcMassMatrix(Body* body, Eigen::MatrixXd& out_M)
 
     if(!rootLink->isFixedJoint()){
         for(int i=0; i < 3; ++i){
-            rootLink->dv()[i] += 1.0;
+            rootLink->dv()[i] = 1.0;
             setColumnOfMassMatrix(body, out_M, i);
-            rootLink->dv()[i] -= 1.0;
+            rootLink->dv()[i] = 0.0;
         }
         for(int i=0; i < 3; ++i){
             rootLink->dw()[i] = 1.0;
