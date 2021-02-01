ac5d219e37474dc3ddc561fe74fa1d26e26b6ae5
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2021-02-02 00:50:21 +0900

    Improve the Python bidning of the ProjectManager class

--- src/Base/pybind11/PyProjectManager.cpp
+++ src/Base/pybind11/PyProjectManager.cpp
@@ -18,8 +18,12 @@ void exportPyProjectManager(py::module m)
     py::class_<ProjectManager>(m, "ProjectManager")
         .def_property_readonly_static(
             "instance", [](py::object){ return ProjectManager::instance(); }, py::return_value_policy::reference)
-        .def("loadProject",
-             &ProjectManager::loadProject, py::arg("filename"), py::arg("parentItem") = nullptr)
+        .def("clearProject", &ProjectManager::clearProject)
+        .def("loadProject", &ProjectManager::loadProject, py::arg("filename"), py::arg("parentItem") = nullptr)
+        .def("isLoadingProject", &ProjectManager::isLoadingProject)
+        .def("saveProject", &ProjectManager::saveProject, py::arg("filename"), py::arg("item") = nullptr)
+        .def("overwriteCurrentProject", &ProjectManager::overwriteCurrentProject)
+        .def_property_readonly("currentProjectDirectory", &ProjectManager::currentProjectDirectory)
         .def("setCurrentProjectName", &ProjectManager::setCurrentProjectName)
         ;
 }
