2cc3478ee397eb2c485dcb9fab0e7166c7ebed1a
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2021-04-28 01:30:12 +0900

    Update the package install script for Ubuntu 20.04

--- misc/script/install-requisites-ubuntu-20.04.sh
+++ misc/script/install-requisites-ubuntu-20.04.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-sudo apt-get install \
+sudo apt-get -y install \
 build-essential \
 cmake-curses-gui \
-libboost-all-dev \
+libboost-dev \
+libboost-system-dev \
+libboost-program-options-dev \
+libboost-iostreams-dev \
+libboost-filesystem-dev \
 libeigen3-dev \
-libeigen3-doc \
+uuid-dev \
 libxfixes-dev \
 libyaml-dev \
 libfmt-dev \
@@ -16,30 +20,17 @@ libpng-dev \
 qt5-default \
 libqt5x11extras5-dev \
 libqt5svg5-dev \
-qt5-style-plugins \
 python3-dev \
 python3-numpy \
-python2.7-dev \
-python-numpy \
-pybind11-dev \
 libassimp-dev \
-libsdformat6-dev \
-libogre-1.9-dev \
 libode-dev \
-libbullet-dev \
-libbullet-extras-dev \
 libfcl-dev \
-libomniorb4-dev \
-libcos4-dev \
-omniidl \
-omniorb-nameserver \
-python-omniorb \
-omniidl-python \
-uuid-dev \
 libpulse-dev \
 libsndfile1-dev \
 libgstreamer1.0-dev \
-libgstreamer-plugins-base1.0-dev \
-lua5.3 \
-liblua5.3-dev \
-lua-posix
+libgstreamer-plugins-base1.0-dev
+
+# libbullet-dev libbullet-extras-dev
+# lua5.3 iblua5.3-dev lua-posix
+# libomniorb4-dev libcos4-dev omniidl omniorb-nameserver python-omniorb omniidl-python
+# libsdformat6-dev libogre-1.9-dev
