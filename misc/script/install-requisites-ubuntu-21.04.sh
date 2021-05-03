1869139208d84e1513210116cfcf06ed39da640b
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2021-05-03 22:30:14 +0900

    Add the package install script for Ubuntu 21.04

---
+++ misc/script/install-requisites-ubuntu-21.04.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+sudo apt-get -y install \
+build-essential \
+cmake-curses-gui \
+libboost-dev \
+libboost-system-dev \
+libboost-program-options-dev \
+libboost-iostreams-dev \
+libboost-filesystem-dev \
+libeigen3-dev \
+uuid-dev \
+libxfixes-dev \
+libyaml-dev \
+libfmt-dev \
+gettext \
+zlib1g-dev \
+libjpeg-dev \
+libpng-dev \
+qtbase5-dev \
+libqt5svg5-dev \
+libqt5x11extras5-dev \
+python3-dev \
+python3-numpy \
+libassimp-dev \
+libode-dev \
+libfcl-dev \
+libpulse-dev \
+libsndfile1-dev \
+libgstreamer1.0-dev \
+libgstreamer-plugins-base1.0-dev
+
+# libbullet-dev libbullet-extras-dev
+# lua5.3 iblua5.3-dev lua-posix
+# libomniorb4-dev libcos4-dev omniidl omniorb-nameserver python-omniorb omniidl-python
+# libsdformat6-dev libogre-1.9-dev
