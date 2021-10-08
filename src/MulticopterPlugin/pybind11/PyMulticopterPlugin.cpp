69b7694d13052656d13ddc10046669b2f1e4ea51
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2021-10-08 22:13:28 +0900

    Add the setAirDefinitionFile function to the MulticopterSimulatorItem class

--- src/MulticopterPlugin/pybind11/PyMulticopterPlugin.cpp
+++ src/MulticopterPlugin/pybind11/PyMulticopterPlugin.cpp
@@ -9,7 +9,8 @@ PYBIND11_MODULE(MulticopterPlugin, m)
     m.doc() = "Choreonoid MulticopterPlugin module";
 
     py::class_<MulticopterSimulatorItem, MulticopterSimulatorItemPtr, SubSimulatorItem>(m, "MulticopterSimulatorItem")
-        .def(py::init<>());
+        .def(py::init<>())
+        .def("setAirDefinitionFile", &MulticopterSimulatorItem::setAirDefinitionFile);
     
     PyItemList<MulticopterSimulatorItem>(m, "MulticopterSimulatorItemList");
 }
