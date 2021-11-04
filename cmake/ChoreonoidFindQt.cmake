39f822414a5b34b5d39e152cb97c1df08e35db5e
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2021-11-04 10:33:04 +0900

    Fix the CMake code on the Qt detection on Windows

--- cmake/ChoreonoidFindQt.cmake
+++ cmake/ChoreonoidFindQt.cmake
@@ -16,24 +16,39 @@ macro(choreonoid_find_qt_package)
       list(APPEND components X11Extras)
     endif()
     cmake_policy(POP)
+
   elseif(MSVC)
-    set(qt_msvc_hint "msvc20??_64")
-    if(MSVC_VERSION GREATER_EQUAL 1910 AND MSVC_VERSION LESS 1920) # VC++2017
-      # Qt provides the msvc 2015 binary for VC++2017
-      set(qt_msvc_hint "msvc2015_64") 
-    elseif(MSVC_VERSION LESS 1930) # VC++2019
-      set(qt_msvc_hint "msvc2019_64") 
-    #elseif(MSVC_VERSION LESS 1940) # VC++2022
-    #  set(qt_msvc_hint "msvc2022_64") 
+
+    list(APPEND qt_msvc_vers 2015)
+    if(MSVC_VERSION GREATER_EQUAL 1910) # VC++2017 or later
+      list(PREPEND qt_msvc_vers 2017)
     endif()
-    file(GLOB qt_hint_dirs "c:/Qt/5.??.?/${qt_msvc_hint}/lib/cmake" "d:/Qt/5.??.?/${qt_msvc_hint}/lib/cmake")
-    list(SORT qt_hint_dirs)
-    list(REVERSE qt_hint_dirs)
+    if(MSVC_VERSION GREATER_EQUAL 1920) # VC++2019 or later
+      list(PREPEND qt_msvc_vers 2019)
+    endif()
+    if(MSVC_VERSION GREATER_EQUAL 1930) # VC++2022 or later
+      list(PREPEND qt_msvc_vers 2022)
+    endif()
+    
+    file(GLOB qt_dirs "c:/Qt/5.??.?")
+    list(REVERSE qt_dirs)
+    foreach(qt_msvc_ver ${qt_msvc_vers})
+      foreach(qt_dir ${qt_dirs})
+	set(qt_cmake_dir "${qt_dir}/msvc${qt_msvc_ver}_64/lib/cmake")
+	if(EXISTS ${qt_cmake_dir})
+	  list(APPEND qt_cmake_dirs ${qt_cmake_dir})
+	endif()
+      endforeach()
+    endforeach()
+    if(NOT qt_cmake_dirs)
+      file(GLOB qt_cmake_dirs "?:/Qt/5.??.?/msvc20??_64/lib/cmake")
+      list(REVERSE qt_cmake_dirs)
+    endif()
   endif()
 
   set(CHOREONOID_QT_COMPILE_DEFINITIONS QT_NO_KEYWORDS QT_NO_OPENGL_ES_2)
 
-  find_package(Qt5 REQUIRED COMPONENTS ${components} HINTS ${qt_hint_dirs})
+  find_package(Qt5 REQUIRED COMPONENTS ${components} HINTS ${qt_cmake_dirs})
   foreach(component ${components})
     list(APPEND libs "Qt5::${component}")
   endforeach()
