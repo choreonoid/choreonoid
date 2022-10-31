66e7ad1882f5bb8f3750932e57bc0fb7bc6ee163
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2022-10-31 09:52:31 +0900

    Add a missing overload of the readEx function defined in EigenArchive

--- src/Util/EigenArchive.h
+++ src/Util/EigenArchive.h
@@ -140,6 +140,13 @@ void readEx(const Mapping& mapping, const std::string& key, Eigen::MatrixBase<De
     readEx(&mapping, key, x);
 }
 
+
+template<typename Derived>
+void readEx(const Mapping& mapping, std::initializer_list<const char*> keys, Eigen::MatrixBase<Derived>& x)
+{
+    readEx(&mapping, keys, x);
+}
+
     
 template<typename Derived>
 Listing* write(Mapping* mapping, const std::string& key, const Eigen::MatrixBase<Derived>& x)
