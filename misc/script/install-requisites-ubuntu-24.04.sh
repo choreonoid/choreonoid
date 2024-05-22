684f732c7ae6cf091565642198c75c66d1775e76
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2024-05-22 22:44:57 +0900

    Add the package install script for Ubuntu 24.04

---
+++ misc/script/install-requisites-ubuntu-24.04.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+sudo --preserve-env=DEBIAN_FRONTEND,TZ \
+apt-get install \
+build-essential \
+cmake-curses-gui \
+libeigen3-dev \
+uuid-dev \
+libxfixes-dev \
+libyaml-dev \
+libfmt-dev \
+gettext \
+zlib1g-dev \
+libzip-dev \
+libjpeg-dev \
+libpng-dev \
+libfreetype-dev \
+qtbase5-dev \
+libqt5x11extras5-dev \
+libqt5svg5-dev \
+qttranslations5-l10n \
+python3-dev \
+python3-numpy \
+libassimp-dev \
+libode-dev \
+libfcl-dev \
+libpulse-dev \
+libsndfile1-dev \
+libgstreamer1.0-dev \
+libgstreamer-plugins-base1.0-dev
