choreonoid/choreonoid

commit ac243ac0825f640b6e17953a3bec2be48ab999e6

Fix item context menus to disable menu items that should not be executed in the continuous update state

src/Base/ItemTreeView.cpp
@@ -129,15 +129,19 @@ void ItemTreeView::Impl::onContextMenuRequested(Item* item, MenuManager& menu)
     auto selectAll = menu.addItem(_("Select all"));
     auto clearSelection = menu.addItem(_("Clear selection"));
 
-    if(!item){
+    bool isContinuousUpdateStateSubTree = item->isContinuousUpdateStateSubTree();
+
+    if(!item || isContinuousUpdateStateSubTree){
         rename->setEnabled(false);
         cut->setEnabled(false);
         copy1->setEnabled(false);
         copy2->setEnabled(false);
+        paste->setEnabled(false);
         check->setEnabled(false);
         uncheck->setEnabled(false);
         toggleCheck->setEnabled(false);
         reload->setEnabled(false);
+        saveAs->setEnabled(false);
         clearSelection->setEnabled(false);
 
     } else {
@@ -163,12 +167,6 @@ void ItemTreeView::Impl::onContextMenuRequested(Item* item, MenuManager& menu)
             copy1->setEnabled(false);
             copy2->setEnabled(false);
         }
-        check->sigTriggered().connect(
-            [this](){ itemTreeWidget->setSelectedItemsChecked(true); });
-        uncheck->sigTriggered().connect(
-            [this](){ itemTreeWidget->setSelectedItemsChecked(false); });
-        toggleCheck->sigTriggered().connect(
-            [this](){ itemTreeWidget->toggleSelectedItemChecks(); });
 
         if(!item->hasAttribute(Item::Reloadable)){
             reload->setEnabled(false);
@@ -191,7 +189,18 @@ void ItemTreeView::Impl::onContextMenuRequested(Item* item, MenuManager& menu)
             [this](){ itemTreeWidget->clearSelection(); });
     }
 
-    if(itemTreeWidget->checkPastable(item)){
+    if(item){
+        clearSelection->setEnabled(true);
+        check->sigTriggered().connect(
+            [this](){ itemTreeWidget->setSelectedItemsChecked(true); });
+        uncheck->sigTriggered().connect(
+            [this](){ itemTreeWidget->setSelectedItemsChecked(false); });
+        toggleCheck->sigTriggered().connect(
+            [this](){ itemTreeWidget->toggleSelectedItemChecks(); });
+    }
+
+    if(itemTreeWidget->checkPastable(item) && !(item && isContinuousUpdateStateSubTree)){
+        paste->setEnabled(true);
         paste->sigTriggered().connect(
             [this](){ itemTreeWidget->pasteItems(); });
     } else {

src/BodyPlugin/BodyMotionItem.cpp
@@ -184,10 +184,17 @@ void BodyMotionItem::initializeClass(ExtensionManager* ext)
     ItemTreeView::customizeContextMenu<BodyMotionItem>(
         [](BodyMotionItem* item, MenuManager& menuManager, ItemFunctionDispatcher menuFunction){
             menuManager.setPath("/").setPath(_("Data conversion"));
-            menuManager.addItem(_("Generate old-format position data items"))->sigTriggered().connect(
-                [item](){ item->motion()->updateLinkPosSeqAndJointPosSeqWithBodyStateSeq(); });
-            menuManager.addItem(_("Restore position data from old-format data items"))->sigTriggered().connect(
-                [item](){ item->motion()->updateBodyStateSeqWithLinkPosSeqAndJointPosSeq(); });
+            auto generate = menuManager.addItem(_("Generate old-format position data items"));
+            auto restore = menuManager.addItem(_("Restore position data from old-format data items"));
+            if(item->isContinuousUpdateStateSubTree()){
+                generate->setEnabled(false);
+                restore->setEnabled(false);
+            } else {
+                generate->sigTriggered().connect(
+                    [item]{ item->motion()->updateLinkPosSeqAndJointPosSeqWithBodyStateSeq(); });
+                restore->sigTriggered().connect(
+                    [item](){ item->motion()->updateBodyStateSeqWithLinkPosSeqAndJointPosSeq(); });
+            }
             menuManager.setPath("/");
             menuManager.addSeparator();
             menuFunction.dispatchAs<Item>(item);

src/BodyPlugin/SimulatorItem.cpp
@@ -479,14 +479,27 @@ void SimulatorItem::initializeClass(ExtensionManager* ext)
     ItemTreeView::customizeContextMenu<SimulatorItem>(
         [](SimulatorItem* item, MenuManager& menuManager, ItemFunctionDispatcher menuFunction){
             menuManager.setPath("/").setPath(_("Simulation"));
-            menuManager.addItem(_("Start"))->sigTriggered().connect(
-                [item](){ item->startSimulation(); });
-            menuManager.addItem(_("Pause"))->sigTriggered().connect(
-                [item](){ item->pauseSimulation(); });
-            menuManager.addItem(_("Resume"))->sigTriggered().connect(
-                [item](){ item->restartSimulation(); });
-            menuManager.addItem(_("Finish"))->sigTriggered().connect(
-                [item](){ item->stopSimulation(true); });
+            auto start = menuManager.addItem(_("Start"));
+            auto pause = menuManager.addItem(_("Pause"));
+            auto resume = menuManager.addItem(_("Resume"));
+            auto finish = menuManager.addItem(_("Finish"));
+
+            if(item->isRunning()){
+                start->setEnabled(false);
+                pause->sigTriggered().connect([item]{ item->pauseSimulation(); });
+                finish->sigTriggered().connect([item]{ item->stopSimulation(true); });
+            } else {
+                start->sigTriggered().connect([item]{ item->startSimulation(); });
+                pause->setEnabled(false);
+                finish->setEnabled(false);
+            }
+            if(item->isPausing()){
+                pause->setEnabled(false);
+                resume->sigTriggered().connect([item]{ item->restartSimulation(); });
+            } else {
+                resume->setEnabled(false);
+            }
+            
             menuManager.setPath("/");
             menuManager.addSeparator();
             menuFunction.dispatchAs<Item>(item);
@@ -2444,6 +2457,7 @@ void SimulatorItem::Impl::stopSimulation(bool isForced, bool doSync)
         }
         isForcedToStopSimulation = isForced;
         stopRequested = true;
+        pauseRequested = false;
         
         if(doSync){
             wait();

commit d5cf0764b4b5af9aab16b23ca58576efed99b6e3

Fix ItemTreeView to avoid a crash when its empty space is right-clicked, which was introduced in commit ac243ac0

src/Base/ItemTreeView.cpp
@@ -129,7 +129,8 @@ void ItemTreeView::Impl::onContextMenuRequested(Item* item, MenuManager& menu)
     auto selectAll = menu.addItem(_("Select all"));
     auto clearSelection = menu.addItem(_("Clear selection"));
 
-    bool isContinuousUpdateStateSubTree = item->isContinuousUpdateStateSubTree();
+    bool isContinuousUpdateStateSubTree =
+        item ? item->isContinuousUpdateStateSubTree() : false;
 
     if(!item || isContinuousUpdateStateSubTree){
         rename->setEnabled(false);
@@ -199,7 +200,7 @@ void ItemTreeView::Impl::onContextMenuRequested(Item* item, MenuManager& menu)
             [this](){ itemTreeWidget->toggleSelectedItemChecks(); });
     }
 
-    if(itemTreeWidget->checkPastable(item) && !(item && isContinuousUpdateStateSubTree)){
+    if(itemTreeWidget->checkPastable(item) && !isContinuousUpdateStateSubTree){
         paste->setEnabled(true);
         paste->sigTriggered().connect(
             [this](){ itemTreeWidget->pasteItems(); });