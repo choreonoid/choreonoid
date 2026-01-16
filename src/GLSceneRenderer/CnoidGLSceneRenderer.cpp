658518716f786a1758d3755ed6d036aa1f3b501a
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2026-01-17 01:01:27 +0900

    Add text domain binding for GLSceneRenderer

---
+++ src/GLSceneRenderer/CnoidGLSceneRenderer.cpp
@@ -0,0 +1,8 @@
+/*! @file
+  @note This file must be linked lastly because the constructors of the static class instances
+  define in this file must be executed after all the other static instances are constructed.
+*/
+
+#include <cnoid/GettextUtil>
+
+CNOID_BIND_MODULE_TEXT_DOMAIN(GLSceneRenderer)
