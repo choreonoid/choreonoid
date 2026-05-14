1243ed00d4947c5980ae6aeb94fe5ca72a3d02ca
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2026-05-14 23:53:40 +0900

    Update Copyright notice for 2026
    - Update year in LICENSE and debian/copyright (2019-2025 -> 2019-2026)
    - Sync the main Copyright entry of debian/copyright with LICENSE
    - Expand debian/copyright to cover all thirdparty libraries bundled
      in the source package
    - Remove the per-file Copyright comment from src/Choreonoid/choreonoid.cpp
      (Copyright is centralized in the LICENSE file)

--- src/Choreonoid/choreonoid.cpp
+++ src/Choreonoid/choreonoid.cpp
@@ -1,10 +1,3 @@
-/*
-  This file is part of Choreonoid, an extensible graphical robotics application suite.
-  Copyright (c) 2019-2024 Choreonoid Inc.
-  Copyright (c) 2007-2019 National Institute of Advanced Industrial Science and Technology (AIST)
-  Released under the MIT license. See 'LICENSE' for more information.
-*/
-
 #include <cnoid/App>
 
 int main(int argc, char *argv[])
