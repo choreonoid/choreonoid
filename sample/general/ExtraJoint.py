89126f776980e2793bcf1555e284ba646bef37ef
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2026-06-17 21:33:28 +0900

    Clean up sample projects to support multiple simulator item types

--- sample/general/ExtraJoint.py
+++ sample/general/ExtraJoint.py
@@ -11,6 +11,10 @@ ej.setLocalTranslation(0, [0.0, 0.0, -0.24])
 ej.setLink(1, box.body.rootLink)
 ej.setLocalTranslation(1, [0.0, 0.0, 0.1])
 
-simulator = RootItem.instance.findItem(AISTSimulatorItem)
-simulator.clearExtraJoints()
-simulator.addExtraJoint(ej)
+# Register the extra joint to each body. When a simulator clones bodies through
+# a shared CloneMap (as AISTSimulatorItem does), the link references inside the
+# extra joint are properly translated to those of the cloned bodies.
+sr1.body.clearExtraJoints()
+sr1.body.addExtraJoint(ej)
+box.body.clearExtraJoints()
+box.body.addExtraJoint(ej)
